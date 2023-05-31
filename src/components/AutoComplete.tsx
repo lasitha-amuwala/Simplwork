23802ab6efb4fef72968145575d5e0686a92cd10
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-05-31 14:01:25 -0400

    Created autocomplete component

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,33 +1,74 @@
 import React, { useState } from 'react';
-import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 import { useCombobox } from 'downshift';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import { SimplworkClient } from '../utils/simplwork';
 import { useAuth } from './Auth/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
 
 type Props = {};
+type ItemType = {
+	place_name: '';
+	center: number[];
+	id?: string;
+};
 
 const AutoComplete = (props: Props) => {
-	const [items, setItems] = useState([]);
-	const { isOpen, getInputProps } = useCombobox({ items });
 	const { user } = useAuth();
 
-	const getResults = () => {
-		if (user?.credential) {
-			SimplworkClient(user?.credential).get('autoSuggest?query=toronto').then(res => console.log(res))
+	const initalItem: ItemType = { place_name: '', center: [] };
+	const [items, setItems] = useState<ItemType[]>([initalItem]);
+	const [selectedItem, setSelectedItem] = useState<ItemType | null>(null);
+
+	const { isOpen, highlightedIndex, getInputProps, getMenuProps, getItemProps, getLabelProps } = useCombobox({
+		items,
+		selectedItem,
+		onInputValueChange: ({ inputValue }) => getResults(inputValue),
+		itemToString: (item) => (item ? item.place_name : ''),
+		onSelectedItemChange: ({ selectedItem: newSelectedItem }) => setSelectedItem(newSelectedItem ?? null),
+	});
+
+	const getResults = async (inputValue?: string) => {
+		if (!inputValue && !inputValue?.length) setItems([initalItem]);
+
+		if (inputValue && inputValue.length > 3 && user?.credential) {
+			const query = inputValue.replaceAll(/[^a-zA-Z0-9 ]/g, '');
+			await SimplworkClient(user?.credential)
+				.get(`geocoding?query=${query}`)
+				.then(({ data }) => setItems(data.features.map(({ place_name, center }: ItemType) => ({ place_name, center }))))
+				.catch((err) => setItems([initalItem]));
 		}
-	}
+	};
 
 	return (
 		<div className='flex flex-col'>
-			<label className='font-medium leading-[35px]'>Home Address:</label>
-			<input
-				{...getInputProps()}
-				className='box-border inline-flex h-[35px] w-full appearance-none items-center justify-center rounded-[4px] px-[10px] leading-none shadow-[0_0_0_1px]  focus:shadow-[0_0_0_2px_black]'
-			/>
-			<button onClick={() => getResults()}>Press</button>
-			<ul>{isOpen && items.map((item, index) => <li key={`${item}${index}`}>hi</li>)}</ul>
+			<label {...getLabelProps()} className='font-medium leading-[35px]'>
+				Home Address:
+			</label>
+			<div>
+				<input
+					{...getInputProps()}
+					className='box-border inline-flex h-9 w-full appearance-none items-center justify-center rounded-[4px] px-[10px] leading-none shadow-[0_0_0_1px]  focus:shadow-[0_0_0_2px_black]'
+				/>
+				<ul
+					{...getMenuProps()}
+					className={`absolute p-0 w-[350px] mt-1 bg-white shadow-md overflow-auto max-h-80 ${!(isOpen && items.length) && 'hidden'}`}>
+					{isOpen &&
+						items.length &&
+						items.map(
+							(item, index) =>
+								isOpen && (
+									<li
+										key={`${item.id}${index}`}
+										className={`truncate p-1 border-b  ${highlightedIndex === index && 'bg-sky-100'} ${
+											selectedItem?.place_name === item.place_name && 'text-sky-500'
+										}`}
+										{...getItemProps({ item, index })}>
+										<span className='font-semibold'>{`${item.place_name.split(',')[0]}`}</span>
+										<span className='text-sm'>{item.place_name.split(',').splice(1).join(',')}</span>
+									</li>
+								)
+						)}
+				</ul>
+			</div>
 		</div>
 	);
 };
