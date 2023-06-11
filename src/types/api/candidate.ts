271a4ab8935638eb81e2c4e5dd86cb9b826a9e91
Lasitha Amuwala <lasithaamuwala@gmail.com>
2023-06-11 03:22:04 -0400

    Update candidate types

--- src/types/api/candidate.ts
+++ src/types/api/candidate.ts
@@ -1,57 +1,59 @@
-interface CandidateWorkHistory {
-	positionTitle: string;
-	companyName: string;
-	details: string;
-	startDate: Date;
-	endDate: Date;
-}
+export type CandidateWorkHistory = {
+  positionTitle: string;
+  companyName: string;
+  details: string;
+  startDate: Date;
+  endDate: Date;
+};
 
-interface CandidateLocation {
-	latitude: number;
-	longitude: number;
-	postalCode: string;
-}
+export type CandidateLocation = {
+  latitude: number;
+  longitude: number;
+  postalCode: string;
+};
 
-interface CandaidateAvailibility {
-	times: [];
-}
+export type CandaidateAvailibility = {
+  times: [];
+};
 
-export interface CandaidateProfile {
-	workHistory: CandidateWorkHistory[] | [];
-	minimumPay: number;
-	minimumHours: number;
-	location: CandidateLocation;
-	availibility?: CandaidateAvailibility;
-	maxLiftWeight: number;
-	maxTravelTimes?: CandidateMaxTravelTimes[];
-	autoMatch: boolean;
-}
+export type CandaidateProfile = {
+  workHistory: CandidateWorkHistory[] | [];
+  minimumPay: number;
+  minimumHours: number;
+  location: CandidateLocation;
+  availibility?: CandaidateAvailibility;
+  maxLiftWeight: number;
+  maxTravelTimes?: CandidateMaxTravelTimes;
+  autoMatch: boolean;
+};
 
-interface CandidateMaxTravelTimes {}
+export type CandidateMaxTravelTimes = {
+  [key: string]: number;
+};
 
-interface User {
-	email: string;
-	age: number;
-	gender: string;
-	phoneNumber: string;
-	name: string;
-}
+export type User = {
+  email: string;
+  age: number;
+  gender: string;
+  phoneNumber: string;
+  name: string;
+};
 
-export interface CandiatePostRequest {
-	candidateProfile: CandaidateProfile;
-	user: User;
-}
+export type CandiatePostRequest = {
+  candidateProfile: CandaidateProfile;
+  user: User;
+};
 
-export interface CandiateGetRequest {
-	profileName: string;
-	candidateName: string;
-	phoneNumber: string;
-	email: string;
-	gender: string;
-	age: number;
-	maxTravelTimes: CandidateMaxTravelTimes[];
-	workHistory: CandidateWorkHistory[];
-	postalCode: string;
-	availibility: CandaidateAvailibility;
-	autoMatch: boolean;
-}
+export type CandiateGetRequest = {
+  profileName: string;
+  candidateName: string;
+  phoneNumber: string;
+  email: string;
+  gender: string;
+  age: number;
+  maxTravelTimes: CandidateMaxTravelTimes;
+  workHistory: CandidateWorkHistory[];
+  postalCode: string;
+  availibility: CandaidateAvailibility;
+  autoMatch: boolean;
+};
