2b8205f685b1bb20b4d5cdbfde43eff88861a228
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-06-20 22:42:03 -0400

    updated layouts

--- src/components/layout/EmptyLayout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactElement } from 'react';
-
-export const EmptyLayout = ({ children }: { children: ReactElement }) => (
-	<div className='flex justify-center items-center min-h-screen h-screen w-full'>{children}</div>
-);
