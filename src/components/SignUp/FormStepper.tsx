5ba6c0273aae9bbb14a9935cd5ae2d2464197d7c
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-06-20 22:43:28 -0400

    Rename FormStep to FormStepper

--- src/components/SignUp/FormStepper.tsx
+++ src/components/SignUp/FormStepper.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Form, FormikConfig, FormikValues } from 'formik';
 import { StepHeader } from './StepHeader';
 
-type FormStepperProps = { step: number };
+type FormStepperProps = { step: number; updateStep: (step: number) => void };
 
-export const FormStepper = ({ children, step }: React.PropsWithChildren<FormStepperProps>) => {
+export const FormStepper = ({ children, step, updateStep }: React.PropsWithChildren<FormStepperProps>) => {
 	const arrayChildren = React.Children.toArray(children) as React.ReactElement<FormikStepProps>[];
 	const currentChild = arrayChildren[step];
 
@@ -12,6 +12,13 @@ export const FormStepper = ({ children, step }: React.PropsWithChildren<FormStep
 		<Form noValidate>
 			<div className='flex flex-col w-[450px] max-w-[450px] gap-5'>{currentChild}</div>
 			<div className='flex w-full pt-7 gap-3'>
+				{step > 0 && (
+					<button
+						onClick={() => updateStep(step - 1)}
+						className='w-full bg-[#64B1EC] p-3 text-white font-medium text-center items-center rounded-[4px] cursor-pointer hover:bg-[#64b1ec]/90 active:bg-[#64b1ec]/80 disabled:bg-gray-300'>
+						Back
+					</button>
+				)}
 				<button
 					type='submit'
 					className='w-full bg-[#64B1EC] p-3 text-white font-medium text-center items-center rounded-[4px] cursor-pointer hover:bg-[#64b1ec]/90 active:bg-[#64b1ec]/80 disabled:bg-gray-300'>
