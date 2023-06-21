b557e85bba2c7e6c18d6cb979f6ea1aa39fe6ca9
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-06-21 13:34:31 -0400

    Fixed font issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { BaseLayout } from '@/src/components/layout/BaseLayout';
+import { Inter } from 'next/font/google';
+import { BaseLayout } from '@/src/layouts/BaseLayout';
 import { NextPageWithLayout } from '@/src/types/NextPageWithLayout';
 import { AuthProvider } from '@/src/components/Auth/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -12,11 +13,15 @@ interface CustomAppProps extends AppProps {
 
 const queryClient = new QueryClient();
 
+const inter = Inter({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: CustomAppProps) {
 	const getLayout = Component.getLayout ?? ((page) => <BaseLayout>{page}</BaseLayout>);
 	return (
 		<QueryClientProvider client={queryClient}>
-			<AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
+			<AuthProvider>
+				<main className={inter.className}>{getLayout(<Component {...pageProps} />)} </main>
+			</AuthProvider>
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	);
