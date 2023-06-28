fa68c2173e06128a3c7d235583d08ea6b87d9872
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-06-28 18:33:20 -0400

    Added user image to profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,22 +1,49 @@
-// import { FriendList } from '@/src/components/Example';
 import { useAuth } from '@/src/components/Auth/AuthProvider';
 import { ProtectedPage } from '@/src/components/Auth/ProtectedPage';
 import { simplwork } from '@/src/utils/simplwork';
 import { useQuery } from '@tanstack/react-query';
 import { NextPage } from 'next';
+import Image from 'next/image';
 import React from 'react';
 
 type Props = {};
 
 const Profile: NextPage = (props: Props) => {
 	const { user } = useAuth();
-	const { data: candidate } = useQuery(simplwork.candidate.getCandidate(user));
+	const { data: candidate } = useQuery(simplwork.candidate.getCandidate(user?.credential ?? ''));
 
 	return (
 		<ProtectedPage>
 			<div className='bg-white rounded-3xl border mt-7 w-full overflow-hidden'>
-				<div className='h-36 bg-gradient-to-r from-white to-[#69BEFF]/60 w-full'></div>
-				<div className='h-36'>{candidate?.candidateName}</div>
+				<div className='relative'>
+					<div className='h-36 bg-gradient-to-r from-white to-[#69BEFF]/60 w-full'></div>
+					<div className='h-36 flex justify-between bg-white py-5 px-5'>
+						<div className='flex flex-col ml-56'>
+							{candidate && <h1 className='text-4xl font-semibold'>{candidate.candidateName}</h1>}
+							<p className='text-lg text-gray-500'>I'm a web developer based in Toronto</p>
+						</div>
+						<div>
+							<button className='bg-black text-white py-2 px-3 rounded-xl'>Edit Profile</button>
+						</div>
+					</div>
+					<div className='absolute top-1/2 -translate-y-1/3 left-10 rounded-full w-[175px] h-[175px] bg-sky-200 overflow-hidden flex justify-center items-center shadow-lg '>
+						<div className='w-[165px] h-[165px] rounded-full relative overflow-hidden'>
+							{user?.picture && (
+								<Image
+									className='object-cover'
+									src={user?.picture.replace('s96-c', 's384-c')}
+									alt='profile picture'
+									fill
+									quality={100}
+									priority
+									placeholder='blur'
+									blurDataURL={user?.picture}
+								/>
+							)}
+						</div>
+					</div>
+				</div>
+				<div></div>
 			</div>
 		</ProtectedPage>
 	);
