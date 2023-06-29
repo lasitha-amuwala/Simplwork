07201521bdacf94e4cb262bf41759ff69d96fbb7
Lasitha Amuwala <lasithaamuwala@gmail.com>
2023-06-28 23:15:45 -0400

    Added link indicators to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ import * as Popover from '@radix-ui/react-popover';
 import * as Avatar from '@radix-ui/react-avatar';
 import { RxCross2 } from 'react-icons/rx';
 import { RiMenuFill } from 'react-icons/ri';
+import { useRouter } from 'next/router';
 
 const NavControls = () => {
 	const { user, signOut } = useAuth();
@@ -62,6 +63,7 @@ const NavControls = () => {
 
 const Navbar = () => {
 	const [open, setOpen] = useState(false);
+	const { pathname } = useRouter();
 
 	const navlinks = [
 		{ name: 'Find Jobs', href: '/' },
@@ -95,13 +97,18 @@ const Navbar = () => {
 				<div>
 					<img src='/Logo-long.svg' alt='Simplwork logo' />
 				</div>
-				<div className='hidden md:block'>
-					<ul className='flex gap-10 font-medium'>
+				<div className='hidden md:block h-full'>
+					<ul className='flex gap-10 font-medium h-full'>
 						{navlinks.map((link) => (
-							<li key={link.name}>
-								<Link className='text-gray-500 hover:text-neutral-800' href={link.href}>
-									{link.name}
-								</Link>
+							<li key={link.name} className='h-full items-center flex flex-col border-sky-500'>
+								<div className='grow flex items-center'>
+									<Link
+										className={`${pathname === link.href ? 'text-sky-500 hover:text-sky-600' : 'text-gray-500 hover:text-neutral-800'} `}
+										href={link.href}>
+										{link.name}
+									</Link>
+								</div>
+								<div className={`${pathname === link.href ? 'visible' : 'invisible'} bg-sky-300 h-[3px] w-14 rounded-t-2xl`}></div>
 							</li>
 						))}
 					</ul>
