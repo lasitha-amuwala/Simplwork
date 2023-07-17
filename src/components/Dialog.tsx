0a7137cc52797cfe88c72f15bdf916392132af9f
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-07-17 19:24:46 -0400

    Created dialog component

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode, useState, PropsWithChildren } from 'react';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { RxCross2 } from 'react-icons/rx';
+
+type DialogContentProps = {};
+type DialogTitleProps = { children: string };
+type DialogDescriptionProps = { children: string };
+
+export const DialogContent = React.forwardRef<HTMLDivElement, PropsWithChildren<DialogContentProps>>(
+	({ children, ...props }, forwardedRef) => {
+		const [open, setOpen] = useState(false);
+
+		return (
+			<DialogPrimitive.Portal>
+				<DialogPrimitive.Overlay className='bg-black/30 data-[state=open]:animate-overlayShow fixed inset-0' />
+				<DialogPrimitive.Content
+					{...props}
+					ref={forwardedRef}
+					className='data-[state=open]:animate-contentShow overflow-auto fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none'>
+					{children}
+					<DialogPrimitive.Close aria-label='Close' asChild>
+						<button
+							className='text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none'
+							aria-label='Close'>
+							<RxCross2 />
+						</button>
+					</DialogPrimitive.Close>
+				</DialogPrimitive.Content>
+			</DialogPrimitive.Portal>
+		);
+	}
+);
+
+export const DialogTitle = ({ children }: DialogTitleProps) => {
+	return <DialogPrimitive.Title className='text-black m-0 text-2xl font-medium'>{children}</DialogPrimitive.Title>;
+};
+
+export const DialogDescription = ({ children }: DialogDescriptionProps) => {
+	return <DialogPrimitive.Description className='text-gray-500'>{children}</DialogPrimitive.Description>;
+};
+
+export const Dialog = DialogPrimitive.Root;
+export const DialogClose = DialogPrimitive.Close;
+export const DialogTrigger = DialogPrimitive.Trigger;
