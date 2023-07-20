4221a09066bfbd2002523151e04ef98386bc63f3
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-07-20 01:53:27 -0400

    Created Dialog component using RadixUI dialog

--- src/components/Dialogs/Dialog.tsx
+++ src/components/Dialogs/Dialog.tsx
@@ -1,22 +1,20 @@
-import React, { ReactNode, useState, PropsWithChildren } from 'react';
+import React, { PropsWithChildren } from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { RxCross2 } from 'react-icons/rx';
 
-type DialogContentProps = {};
+type DialogContentProps = { className?: string };
 type DialogTitleProps = { children: string };
 type DialogDescriptionProps = { children: string };
 
-export const DialogContent = React.forwardRef<HTMLDivElement, PropsWithChildren<DialogContentProps>>(
-	({ children, ...props }, forwardedRef) => {
-		const [open, setOpen] = useState(false);
-
+const DialogContent = React.forwardRef<HTMLDivElement, PropsWithChildren<DialogContentProps>>(
+	({ className, children, ...props }, forwardedRef) => {
 		return (
 			<DialogPrimitive.Portal>
 				<DialogPrimitive.Overlay className='bg-black/30 data-[state=open]:animate-overlayShow fixed inset-0' />
 				<DialogPrimitive.Content
 					{...props}
 					ref={forwardedRef}
-					className='data-[state=open]:animate-contentShow overflow-auto fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none'>
+					className={`${className} data-[state=open]:animate-contentShow overflow-auto fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none`}>
 					{children}
 					<DialogPrimitive.Close aria-label='Close' asChild>
 						<button
@@ -39,6 +37,8 @@ export const DialogDescription = ({ children }: DialogDescriptionProps) => {
 	return <DialogPrimitive.Description className='text-gray-500'>{children}</DialogPrimitive.Description>;
 };
 
+DialogContent.displayName = 'DialogContent';
+export default DialogContent;
 export const Dialog = DialogPrimitive.Root;
 export const DialogClose = DialogPrimitive.Close;
 export const DialogTrigger = DialogPrimitive.Trigger;
