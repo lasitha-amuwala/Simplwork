093509498df87f3a29b53cef5e8e3306e8ff88d7
Lasitha Amuwala <lasithaamuwala@gmail.com>
2023-08-02 10:44:26 -0400

    updated applications page

--- src/components/Skeletons/PostItemSkeleton.tsx
+++ src/components/Skeletons/PostItemSkeleton.tsx
@@ -3,11 +3,11 @@ export const PostItemSkeleton = () => (
 		<div className='h-[50px] w-[50px] bg-gray-300 rounded col-span-2 row-span-2'></div>
 		<div className='h-5 bg-gray-300 rounded col-span-7'></div>
 		<div className='h-4 bg-gray-300 rounded col-span-6'></div>
-		<div className='h-7 bg-gray-300 rounded col-span-2 col-start-1'></div>
-		<div className='h-7 bg-gray-300 rounded col-span-2'></div>
-		<div className='h-7 bg-gray-300 rounded col-span-2'></div>
-		<div className='h-2 bg-gray-300 rounded col-span-12'></div>
-		<div className='h-2 bg-gray-300 rounded col-span-12'></div>
-		<div className='h-2 bg-gray-300 rounded col-span-12'></div>
+		<div className='h-9 bg-gray-300 rounded-full col-span-2 col-start-1'></div>
+		<div className='h-9 bg-gray-300 rounded-full col-span-2'></div>
+		<div className='h-9 bg-gray-300 rounded-full col-span-2'></div>
+		<div className='h-3 bg-gray-300 rounded col-span-12'></div>
+		<div className='h-3 bg-gray-300 rounded col-span-12'></div>
+		<div className='h-2 bg-gray-300 rounded col-span-3'></div>
 	</div>
 );
