a9d45d5d61427cf8158d8e8764a2d10cec52bf83
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-08-24 11:23:40 -0400

    fixed loading and error states for applications

--- src/components/Applications/MatchStatusList.tsx
+++ src/components/Applications/MatchStatusList.tsx
@@ -1,8 +1,9 @@
 import { PostCard, PostCardSkeleton } from '@components/Posts/PostCard';
-import { useQuery } from '@tanstack/react-query';
+import { isError, useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 import { queries } from '@utils/simplwork';
 import { useAuth } from '@components/Auth/AuthProvider';
+import { MdError, MdErrorOutline } from 'react-icons/md';
 interface CandidateMatchResponse {
 	posting: SW.IPosting;
 	candidateStatus: string;
@@ -11,7 +12,7 @@ interface CandidateMatchResponse {
 export const MatchStatusList = ({ status, label }: { status: string; label: string }) => {
 	const { user } = useAuth();
 
-	const { data, isSuccess, isLoading } = useQuery<CandidateMatchResponse[]>(
+	const { data, isError, isLoading } = useQuery<CandidateMatchResponse[]>(
 		queries.candidate.getCandidatePostings(user?.credential as string, { requestStatusSet: status })
 	);
 
@@ -19,24 +20,31 @@ export const MatchStatusList = ({ status, label }: { status: string; label: stri
 		<div>
 			<h1 className='text-3xl pl-1 font-bold'>{data?.length ? `${label} (${data.length})` : `${label} (0)`}</h1>
 			<div className='flex gap-3 pt-5 overflow-x-auto p-1'>
-				{isLoading &&
+				{isLoading ? (
 					[...Array(3).fill(0)].map((key, i) => (
 						<div key={`${key}-${i}`} className='min-w-[350px] w-full'>
 							<PostCardSkeleton />
 						</div>
-					))}
-				{isSuccess &&
-					(data.length > 0 ? (
-						data?.map(({ posting, candidateStatus }) => (
-							<Link href={{ pathname: '/applications', query: { id: posting.id } }} key={`${posting.id}`} className='min-w-[350px] w-full'>
-								<PostCard post={posting} status={candidateStatus} />
-							</Link>
-						))
-					) : (
-						<div className='bg-gray-200 w-full py-10 px-5 flex flex-col justify-center items-center rounded'>
-							<p className='py-10'>No applications.</p>
-						</div>
-					))}
+					))
+				) : isError ? (
+					<div className='bg-gray-200 rounded-md w-full h-[218px] py-10 px-5 flex justify-center items-center text-xl font-medium gap-2'>
+						<MdErrorOutline />
+						There was an Error...
+					</div>
+				) : data.length > 0 ? (
+					data?.map(({ posting, candidateStatus }) => (
+						<Link
+							href={{ pathname: '/applications', query: { status: status.toLowerCase(), id: posting.id } }}
+							key={`${posting.id}`}
+							className='min-w-[350px] w-full'>
+							<PostCard post={posting} status={candidateStatus} />
+						</Link>
+					))
+				) : (
+					<div className='bg-gray-200 rounded-md w-full h-[218px] py-10 px-5 flex flex-col justify-center items-center'>
+						<p className='py-10'>No applications.</p>
+					</div>
+				)}
 			</div>
 		</div>
 	);
