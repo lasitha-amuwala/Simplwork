68c22abe8392689c883396cabc57a71f36659440
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-09-05 05:54:06 -0400

    Created delete dialog, and delete posting dialog

---
+++ src/components/Dialogs/DeleteDialog.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import { MdDelete } from 'react-icons/md';
+import { DialogContentLayout } from '@components/Dialogs/DialogContentLayout';
+import { DialogCancelButton } from './DialogCancelButton';
+import { CgSpinner } from 'react-icons/cg';
+
+type DeleteDialogProps = { onDelete: () => void; isLoading: boolean; isError: boolean; isSuccess: boolean };
+export const DeleteDialog = ({ onDelete, isLoading, isSuccess }: DeleteDialogProps) => {
+	const [open, setOpen] = useState(false);
+
+	useEffect(() => {
+		if (isSuccess) setOpen(false);
+	}, [isSuccess]);
+
+	return (
+		<DialogContentLayout
+			open={open}
+			setOpen={setOpen}
+			triggerLabel={<MdDelete />}
+			triggerClassName='p-2 rounded baby-red-btn'
+			title='Are You Sure?'
+			description={`This cannot be undone.`}>
+			<div className='flex gap-5 justify-center p-5 pb-0'>
+				<DialogCancelButton />
+				<button
+					onClick={onDelete}
+					disabled={isLoading}
+					type='submit'
+					className='group inline-flex justify-center items-center baby-red-btn px-4 py-2 font-medium group-disabled:pointer-events-none rounded'>
+					<CgSpinner className='w-5 h-5 absolute group-enabled:opacity-0 animate-spin' />
+					<span className='group-disabled:opacity-0'>Delete</span>
+				</button>
+			</div>
+		</DialogContentLayout>
+	);
+};
---
+++ src/components/Dialogs/DeletePostingDialog.tsx
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { SimplworkApi } from '@utils/simplwork';
+import { DeleteDialog } from './DeleteDialog';
+
+export const DeletePostingDialog = ({ id }: { id: number }) => {
+	const queryClient = useQueryClient();
+
+	const deletePost = (id: number) => SimplworkApi.delete(`employer/postings/${id}`);
+
+	const { mutate, isError, isLoading, isSuccess } = useMutation({
+		mutationFn: deletePost,
+		onSuccess: () => queryClient.invalidateQueries(),
+		onError: () => alert('There was an issue deleting your postings, please try again later.'),
+	});
+
+	return <DeleteDialog onDelete={() => mutate(id)} isLoading={isLoading} isError={isError} isSuccess={isSuccess} />;
+};
