00c72cd8f79e5bb96691667e547715e3d20b95b3
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-09-06 10:12:37 -0400

    Removed old availability widget

--- src/components/AvailabilityWidget/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './helpers';
-export * from './AvailabilityWidget';
-export * from './AvailabilityEdit';
--- src/components/AvailabilityWidget/types.ts
+++
@@ -1,6 +0,0 @@
-export interface AvailabilityWidgetProps {
-	hourlyChunks?: number;
-	readonly?: boolean;
-	availability?: SW.IAvailability;
-	onChange?: (arg: SW.IAvailability) => void;
-}
