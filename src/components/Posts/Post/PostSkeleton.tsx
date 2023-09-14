11fa3ca25b0ef0f825a277ef5c6376a8f90a74d3
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-09-14 04:04:07 -0400

    Updates post skeletons

--- src/components/Posts/Post/PostSkeleton.tsx
+++ src/components/Posts/Post/PostSkeleton.tsx
@@ -1,11 +1,13 @@
+import { Card } from "@components/Card";
+
 export const PostSkeleton = () => (
-	<div className='bg-white rounded-md border border-gray-200 animate-pulse mt-1'>
-		<div className='p-4 grid grid-cols-12 gap-3'>
+	<Card className='bg-white rounded-md border border-gray-200 animate-pulse mt-1'>
+		<div className=' grid grid-cols-12 gap-3'>
 			<div className='h-20 w-20 bg-gray-300 rounded col-span-2 row-span-2' />
 			<div className='h-5 bg-gray-300 rounded col-span-7' />
 			<div className='h-4 bg-gray-300 rounded col-span-6' />
 		</div>
-		<div className='p-4 gap-3 grid grid-cols-12'>
+		<div className='gap-3 grid grid-cols-12'>
 			<div className='h-4 bg-gray-300 rounded col-span-4 col-start-1' />
 			<div className='h-2 bg-gray-300 rounded col-span-12' />
 			<div className='h-2 bg-gray-300 rounded col-span-12' />
@@ -20,5 +22,5 @@ export const PostSkeleton = () => (
 			<div className='h-2 bg-gray-300 rounded col-span-12' />
 			<div className='h-2 bg-gray-300 rounded col-span-12' />
 		</div>
-	</div>
+	</Card>
 );
--- src/components/Posts/PostPreview/PostPreviewSkeleton.tsx
+++ src/components/Posts/PostPreview/PostPreviewSkeleton.tsx
@@ -1,19 +1,21 @@
+import { Card } from '@components/Card';
+
 export const PostPreviewSkeleton = () => (
-	<div className='bg-white rounded-md border border-gray-200 p-4 grid grid-cols-12 gap-3 animate-pulse'>
+	<Card className='grid grid-cols-12 p-4 gap-3 animate-pulse'>
 		<div className='w-full col-span-12 grid grid-cols-12'>
-			<div className='col-span-7 w-full grid grid-cols-10 gap-3'>
+			<div className='col-span-7 w-full grid grid-cols-10 gap-2'>
 				<div className='h-5 bg-gray-300 rounded col-span-7'></div>
-				<div className='h-4 bg-gray-300 rounded col-span-4 col-start-1'></div>
+				<div className='h-5 bg-gray-300 rounded col-span-4 col-start-1'></div>
 			</div>
 			<div className='h-7 bg-gray-300 rounded col-span-2 col-start-11'></div>
 		</div>
 		<div className='h-9 bg-gray-300 rounded-full col-span-3 col-start-1'></div>
 		<div className='h-9 bg-gray-300 rounded-full col-span-3'></div>
 		<div className='h-9 bg-gray-300 rounded-full col-span-3'></div>
-		<div className='w-full col-span-12 gap-3 grid grid-cols-12'>
-			<div className='h-[18px] bg-gray-300 rounded col-span-12'></div>
-			<div className='h-[18px] bg-gray-300 rounded col-span-12'></div>
+		<div className='h-12 w-full col-span-12 grid grid-cols-12 justify-center items-center'>
+			<div className='h-[75%] bg-gray-300 rounded col-span-12'></div>
+			<div className='h-[75%] bg-gray-300 rounded col-span-12'></div>
 		</div>
 		<div className='h-4 bg-gray-300 rounded col-span-3'></div>
-	</div>
+	</Card>
 );
