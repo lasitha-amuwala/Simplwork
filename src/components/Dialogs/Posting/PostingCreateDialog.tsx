415b9bd2a7340367eeda783ab4773f5b42179040
Lasitha Amuwala <lasithaamuwala@gmail.com>
2023-09-19 15:51:09 -0400

    Added alert for posting without any branches

--- src/components/Dialogs/Posting/PostingCreateDialog.tsx
+++ src/components/Dialogs/Posting/PostingCreateDialog.tsx
@@ -1,12 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DialogContentLayout } from '../DialogContentLayout';
 import { HiOutlinePlus } from 'react-icons/hi';
 import { PostingCreateForm } from './PostingCreateForm';
+import { queries } from '@utils/simplwork';
+import { useAuth } from '@components/Auth/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
 
 type Props = {};
 
 export const PostingCreateDialog = (props: Props) => {
+	const { user, employerName } = useAuth();
 	const [open, setOpen] = useState<boolean>(false);
+
+	const {
+		data: branches,
+		isError,
+		isLoading,
+	} = useQuery(queries.employer.getBranches(user?.credential ?? '', employerName as string, { pageSize: '20', pageNo: '0' }));
+
+	const handleOpen = (isOpen: boolean) => {
+		if (isOpen) {
+			if (branches && branches?.length > 0) {
+				setOpen(true);
+			} else {
+				alert('You must have a branch before creating a job posting');
+			}
+		} else {
+			setOpen(false);
+		}
+	};
 
 	const TriggerButton = () => (
 		<div className='flex gap-2 items-center'>
@@ -18,7 +40,7 @@ export const PostingCreateDialog = (props: Props) => {
 	return (
 		<DialogContentLayout
 			open={open}
-			setOpen={setOpen}
+			setOpen={handleOpen}
 			triggerLabel={<TriggerButton />}
 			title='Create Job Posting'
 			description={`Create and post job posting. Click save when you're done.`}>
